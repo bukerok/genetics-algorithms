76119bb733e24bc4da61c943424a6027ff657663
Alex B <bukerok@gmail.com>
2016-12-28 13:21:23 +0300

    add parents pairs

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var config = require('./config.json');
 var data = require('./data.json');
 var Func = require('./func_generator').Func;
 
-var i;
+var i, j;
 var population = [];
 var parents = [];
+var pairs = [];
 
 var printResult = function (f) {
     //TODO output values to the file
@@ -35,8 +36,76 @@ var funcComparator = function (a, b) {
     }
 };
 
-var getSelectionDataStructure = function (arr) {
+var getFuncMetric = function (func) {
+    var j;
+    var metr = 0;
 
+    for (j = 0; j < data.length; j++) {
+        metr += Math.abs(func.evaluate(data[j].arg) - data[j].val);
+    }
+
+    return metr;
+};
+
+var getSelection = function (arr) {
+    var j, k;
+    var funcMetric;
+    var randInd, indSum;
+    var sumNorm = 0;
+    var finiteFunctions = [];
+    var selection = [];
+
+    for (j = 0; j < arr.length; j++) {
+        funcMetric = getFuncMetric(arr[j]);
+
+        if (!Number.isFinite(funcMetric)) {
+            console.error('UNEXPECTED BEHAVIOR: PROVIDED FUNCTION DISCONTINUOUS.');
+            process.exit(1);
+        }
+
+        sumNorm += funcMetric;
+        finiteFunctions.push({
+            func: arr[j],
+            norm: funcMetric
+        });
+    }
+
+    for (j = 0; j < finiteFunctions.length; j++) {
+        finiteFunctions[j].norm = finiteFunctions[j].norm / sumNorm;
+    }
+
+    for (j = 0; j < Math.floor(config.populationSize / 2); j++) {
+        randInd = Math.random();
+        indSum = 0;
+
+        for (k = 0; k < finiteFunctions.length; k++) {
+            if (randInd < finiteFunctions[k].norm) {
+                // console.log('SELECT', k);
+                break;
+            }
+            indSum += finiteFunctions[k].norm;
+        }
+    }
+
+    return selection;
+};
+
+var shuffle = function (array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 };
 
 // creating initial population
@@ -44,8 +113,12 @@ for (i = 0; i < config.populationSize; i++) {
     population.push(new Func());
 }
 
+for (i = 0; i < Math.floor(config.populationSize / 2); i++) {
+    pairs.push(i);
+}
 
-for (i = 0; i < config.iterations; i++) {
+
+for (i = 0; i < 10; i++) {//config.iterations; i++) {
     parents = [];
 
     //sorting
@@ -58,7 +131,17 @@ for (i = 0; i < config.iterations; i++) {
         printResult(population[population.length - 1]);
     }
     // selection
+    parents = getSelection(population);
     // parents pairs formation
+    pairs = shuffle(pairs);
     // crossover
+    population = [];
+    for (j = 0; j < pairs.length; j++) {
+        population.push(parents[j].crossover(parents[pairs[j]]));
+    }
     // mutation
 }
+
+population.sort(funcComparator);
+printResult(population[0]);
+printResult(population[population.length - 1])
