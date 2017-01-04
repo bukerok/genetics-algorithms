ffa5fed6106fdb35d42da71ffea7c299757e83f1
axelSok <Sokolovsky1712>
2017-01-05 01:43:59 +0300

    parse date on client-side

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var connect = require('connect'),
   path = require("path"),
   app = connect();
 
+var data = [require('./logs/data-0.json'), require('./logs/data-1.json'), require('./logs/data-final.json')];
+
 app.use(serveStatic(__dirname)).listen(8080, function () {
   console.log('Server running on 8080...');
 });
 app.use('/scripts', serveStatic(__dirname + '/node_modules/'));
 app.use('/', serveStatic(__dirname + '/index.html'));
+app.use('/data', function(req, res){
+  res.end(JSON.stringify(data));
+});
